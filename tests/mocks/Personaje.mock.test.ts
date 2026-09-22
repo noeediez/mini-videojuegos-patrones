06ae088505b638518e83809a9f6d4ceff09427f2
noeediez <noediezxd@gmail.com>
2026-09-22 09:19:30 +0000

    .

--- tests/mocks/Personaje.mock.test.ts
+++ tests/mocks/Personaje.mock.test.ts
@@ -10,10 +10,9 @@ class PersonajePrueba extends Personaje {
 describe('Mocks - Personaje y EstrategiaAtaque', () => {
 
   test('Personaje utiliza la estrategia de ataque proporcionada', () => {
-    const estrategiaMock: EstrategiaAtaque = {
-      atacar: jest.fn().mockReturnValue(999),
-    };
-
+    const estrategiaMock: EstrategiaAtaque = { //mocks prueba otra pieza personaje pero reemplazando su dependencia por una falsa, no importa si ataque espada funciona bien 
+      atacar: jest.fn().mockReturnValue(999), //sino que personaje lo use correctamente. y sin strategy el mock no se podria escribir asi de limpio 
+    };                                       //porque no habria que implementar la interfaz EstrategiaAtaque para poder usarlo en el constructor de PersonajePrueba
     const personaje = new PersonajePrueba(estrategiaMock);
 
     const danio = personaje.atacar();
